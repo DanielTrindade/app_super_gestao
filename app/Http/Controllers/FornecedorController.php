2637ca46b4f1555e86ea3693eaaf48ac831c2a4a
Daniel Trindade <danieloliveiratrindade@gmail.com>
2021-11-16 12:04:04 -0400

    add beleza nas paginas

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,25 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => ['nome' => 'fornecedor 1', 
+            'status' => 'N',
+            'cnpj' => '38.554.130/0001-49',
+            'ddd' => '11',
+            'telefone' => '00000-0000'], 
+            1 => ['nome' => 'fornecedor 2', 
+            'status' => 'S',
+            'cnpj' => '40.176.110/0001-23',
+            'ddd' => '32',
+            'telefone' => '91546-3597'],
+            2 => ['nome' => 'fornecedor 3', 
+            'status' => 'N',
+            'cnpj' => '41.716.011/0001-32',
+            'ddd' => '92',
+            'telefone' => '98426-9710']
+        ];
+        echo isset($fornecedores[1]['cnpj']) ? 'CNPJ informado': 'CNPJ não informado';
+
+        return view('app.fornecedor.index',compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,5 +1,70 @@
-<head>Fornecedor</head>
+<h3>Fornecedores</h3>
+
+{{-- IF ELSE ELSEIF--}}
+
+{{-- @if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados!!</h3>
+@elseif(count($fornecedores) >= 10) 
+    <h3>Existem vários fornecedores cadastrados!</h3>
+@else 
+    <h3>Não existem fornecedores cadastrados!</h3>
+@endif --}}
+
+{{-- isset verifica se uma variavel está definida--}}
+@isset($fornecedores)
+    @forelse ($fornecedores as $fornecedor) 
+        iteração atual: {{$loop->iteration}}
+        <br>
+        Fornecedor: {{$fornecedor['nome']}}
+        <br>
+        Status: {{$fornecedor['status']}}
+        <br>
+        CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não preenchido'}}
+        <br>
+        DDD: {{$fornecedor['ddd']?? ''}} Telefone: {{$fornecedor['telefone']}}
+        <br>
+        Cidade: 
+        @switch($fornecedor['ddd'])
+            @case('11')
+                São Paulo - SP
+                @break
+            @case('92')
+                Manaus - AM
+                @break
+            @case('32')
+                Juiz de Fora - MG
+            @break
+            @default
+                Estado não identificado
+        @endswitch
+        <br>
+        @if ($loop->first)
+           Primeira iteração do loop 
+        @endif
+        @if ($loop->last)
+            Ultima iteração do loop <br>
+            Total de registros {{$loop->count}}
+        @endif
+        <hr> 
+    @empty
+        Não existem dados cadastrados!!
+    @endforelse
+@endisset
+
+{{-- 
+@if($fornecedores[0]['status'] == 'N') 
+    <h3>Fornecedor inativo</h3>
+@else 
+    <h3>Fornecedor ativo</h3>
+@endif --}}
+
+{{--usando o comando unless (quando um resultado for falso)--}}
+{{-- <h3>
+    @unless ($fornecedores[0]['nome'] == 'S')
+        Fornecedor inativo
+    @endunless
+</h3> --}}
 
 
 
-{{-- Comentário ignorado pelo laravel --}}
+
